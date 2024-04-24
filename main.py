bd95326f35f49bc5721509b58ff1988293969bfe
tjrelyts <tylerj-santo@outlook.com>
2024-04-24 15:36:32 -0400

    Fixed issue with audio feedback. Program works as intended without encryption and decryption.

--- main.py
+++ main.py
@@ -1,132 +1,60 @@
-import os
-
-import numpy
-import pyaudio
-import numpy as np
-
-from crypto.Random import get_random_bytes
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-from cryptography.hazmat.backends import default_backend
-
-CHUNK = 1024
-RATE = 44100
-
-# Encryption key
-encryption_key = os.urandom(16)
+import sounddevice as sd
+import numpy as np # Make sure NumPy is loaded before it is used in the callback
 
 def select_input(device):
-    devices = p.get_device_count()
+    devices = sd.query_devices()
     input_device = None
-    for i in range(devices):
-        if p.get_device_info_by_index(i)["name"] == device:
+    for i, dev in enumerate(devices):
+        if dev["name"] == device:
             input_device = i
             break
     return input_device
 
 def select_output(device):
-    devices = p.get_device_count()
+    devices = sd.query_devices()
     output_device = None
-    for i in range(devices):
-        if p.get_device_info_by_index(i)["name"] == device:
+    for i, dev in enumerate(devices):
+        if dev["name"] == device:
             output_device = i
             break
     return output_device
 
-def encrypt_data(data, encryption_key):
-    backend = default_backend()
-    iv = get_random_bytes(16)  # Generate a random IV (Initialization Vector)
-    cipher = Cipher(algorithms.AES(encryption_key), modes.CBC(iv), backend=backend)
-    encryptor = cipher.encryptor()
-    ct = encryptor.update(data) + encryptor.finalize()
-    return iv + ct
+# This method is dependent on owning Virtual Audio Cable (VAC)
+def get_virtual_input():
+    devices = sd.query_devices()
+    virtual_input = None
+    for i, dev in enumerate(devices):
+        if dev["name"] == "Line 1 (Virtual Audio Cable)":
+            virtual_input = i
+            break
+    return virtual_input
 
-def decrypt_data(data, encryption_key):
-    iv = data[:16]  # Extract the IV from the encrypted data
-    data = data[16:]  # Remove the IV from the encrypted data
-    backend = default_backend()
-    cipher = Cipher(algorithms.AES(encryption_key), modes.CBC(iv), backend=backend)
-    decryptor = cipher.decryptor()
-    pt = decryptor.update(data) + decryptor.finalize()
-    return pt
+# This method is dependent on owning VB-Audio Virtual Cable 
+def get_virtual_output():
+    devices = sd.query_devices()
+    virtual_output = None
+    for i, dev in enumerate(devices):
+        if dev["name"] == "CABLE Input (VB-Audio Virtual C":
+            virtual_output = i
+            break
+    return virtual_output
 
-#Finds the average volume of the data.
-def average_of_data(data):
-    if(np.mean(data) >= 2):
-        return True
-    return False
+virtual_input, virtual_output = get_virtual_input(), get_virtual_output()
+input_device = select_input('Microphone (HyperX Cloud Alpha ') # This will link to front-end
+output_device = select_output('Headset Earphone (HyperX Cloud ') # This will link to front-end
 
-# PyAudio setup
-p = pyaudio.PyAudio()
-p2 = pyaudio.PyAudio()
+virtual_stream = sd.Stream(device=(virtual_input, virtual_output), channels=2, samplerate=44100)
+physical_stream = sd.Stream(device=(input_device, output_device), channels=2, samplerate=44100)
 
-devices = p.get_device_count()
+virtual_stream.start()
+physical_stream.start()
 
-#Printing available audio devices
-for i in range(p.get_device_count()):
-    print(p.get_device_info_by_index(i))
-
-index1 = 1  # Selected Microphone
-index2 = 5 # VB Audio Cable "Input"
-index3 = 6  # Selected Speaker
-index4 = 2 # VB Audio Cable "Output"
-
-print()
-print(p.get_device_info_by_index(index1))
-print(p.get_device_info_by_index(index2))
-print()
-print(p.get_device_info_by_index(index3))
-print(p.get_device_info_by_index(index4))
-
-# Stream setup
-stream = p.open(
-    format=pyaudio.paInt16, channels=1, rate=RATE, input=True, frames_per_buffer=CHUNK, input_device_index=index1
-)
-2
-player = p.open(
-    format=pyaudio.paInt16, channels=1, rate=RATE, output=True, frames_per_buffer=CHUNK, output_device_index=index2
-)
-
-stream2 = p2.open(
-    format=pyaudio.paInt16, channels=2, rate=RATE, input=True, frames_per_buffer=CHUNK, input_device_index=index4
-)
-
-player2 = p2.open(
-    format=pyaudio.paInt16, channels=2, rate=RATE, output=True, frames_per_buffer=CHUNK, output_device_index=index3
-)
-
-
-# Main loop
-try:
+def read_write_devices():
     while True:
-        # Read data from input streams
-        data  = np.frombuffer(stream.read(CHUNK),  dtype=np.int16)
-        data2 = np.frombuffer(stream2.read(CHUNK), dtype=np.int16)
+        data_physical, _ = physical_stream.read(1024)
+        data_virtual, _ = virtual_stream.read(1024)
+        
+        physical_stream.write(data_virtual)
+        virtual_stream.write(data_physical)
 
-        print(f'Data 1: {data} Avg: {numpy.mean(data)}')
-        print(f'Data 2: {data2} Avg: {numpy.mean(data2)}')
-        print()
-
-        #encrypted_data = encrypt_data(data.tobytes(), encryption_key)
-        #player.write(bytes(encrypted_data), CHUNK)
-        player.write(bytes(data), CHUNK)
-
-        # Check if data is received before decryption
-        #if average_of_data(data2):
-        #decrypted_data = decrypt_data(data2.tobytes(), encryption_key)
-        #player2.write(bytes(decrypted_data), CHUNK)
-        player2.write(bytes(data2), CHUNK)
-
-
-except KeyboardInterrupt:
-    print("Exiting...")
-    # Cleanup
-    stream.stop_stream()
-    stream.close()
-    stream2.stop_stream()
-    stream2.close()
-
-    player.stop_stream()
-    player.close()
-    player2.stop_stream()
-    player2.close()
-    p.terminate()
+read_write_devices()
